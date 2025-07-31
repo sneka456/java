4c4324dd31cf13b0a1e42ae37e593b19001724ea
sneka456 <snehamano3@gmail.com>
2025-07-31 21:46:57 +0530

    Add files via upload

---
+++ weeks.java
@@ -0,0 +1,29 @@
+public class example{
+    public static void main(String[] args){
+        int a =Scanner.nextInt();
+        switch(a);
+        case 1:
+            System.out.println("monday");
+            break;
+        case 2:
+            System.out.println("tuesday");
+            break;
+        case 3:
+            System.out.println("wednesday");
+            break;
+        case 4:
+            System.out.println("thurshday");
+            break;
+        case 5:
+            System.out.println("friday");
+            break;
+        case 6:
+            System.out.println("saturaday");
+            break;
+        case 7:
+            System.out.println("sunday");
+            break;
+            default:
+                System.out.println("Invalid");
+    }
+}
