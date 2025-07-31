3e09ce61414283174f40bbeef61ace8cf8793e34
sneka456 <snehamano3@gmail.com>
2025-07-31 21:47:13 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,9 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int number = 2; 
+        while (number <= 50) {
+            System.out.println(number);
+            number += 2;
+        }
+    }
+}
