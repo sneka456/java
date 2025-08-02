4992fe559c32b51a009532b0727339e63c67fd22
sneka456 <snehamano3@gmail.com>
2025-08-02 09:39:39 +0530

    Add files via upload

---
+++ duplicate.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+        int arr1[]={10,20,30,40};
+        int arr2[]={40,50,60};
+        System.out.println("Duplicate elements: ");
+        for(int i=0;i<arr.length;i+=){
+            for(int j=1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    System.out.println(arr[i]);
+                }
+            }
+        }
+    }
+}
