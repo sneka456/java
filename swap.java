8fdc1cd08bf01665747efddd03a82e7e22bb670e
sneka456 <snehamano3@gmail.com>
2025-08-02 09:40:28 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class SwapIndex {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        arr[3] = 6;
+        System.out.println("Updated array: " + Arrays.toString(arr));
+    }
+}
